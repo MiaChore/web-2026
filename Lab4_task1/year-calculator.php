a7a27e08831999ef61bd1165bed3bce1ca19a356
MiaChore <makskr190@gmail.com>
2026-03-15 13:28:12 +0300

    web-2026

---
+++ Lab4_task1/year-calculator.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="styles.css" rel="stylesheet">
+  <title>Calculator</title>
+</head>
+<body>
+  <div>
+    <h1>Хеллоу, май диар, проверяющий</h1>
+</div>
+  <div>
+    <h2>
+        <?php
+
+            $year = $_POST['year'];
+            if (empty($year)) {
+                print "А где год..." . "<br>";
+                print '<img src="/media/no-year.gif" alt="Форма пустая">';
+            } else {
+                if ($year % 400 == 0) {
+                    print "Год високосный" . "<br>";
+                    print '<img src="/media/yes.gif" alt="Всё верно">';
+                } else {
+                    if ($year % 4 == 0) {
+                        print "Год високосный" . "<br>";
+                        print '<img src="/media/yes.gif" alt="Всё верно">';
+                    } else {
+                        print "Год не високосный" . "<br>";
+                        print '<img src="/media/no.gif" alt="Всё верно">';
+                    }
+                }
+            }
+        ?>
+    </h2>
+</div>
+</body>
+</html>
