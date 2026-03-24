31b422042876c187e420e34a0066c0a98d556832
MiaChore <makskr190@gmail.com>
2026-03-24 20:36:44 +0300

    web-2026

---
+++ Lab5_Task1/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your sign</title>
+</head>
+<body>
+    <div>
+        <?php
+
+            $posts = [
+                [
+                    'title' => 'title',
+                    'subtitle' => 'subtitle',
+                    'img_modifier' => '',
+                    'author' => 'me',
+                    // другие свойства этого поста
+                ],
+                [
+                    'title' => 'New Album',
+                    'subtitle' => 'Самый желанный из меню<br>Я никогда не изменю',
+                    'img_modifier' => '',
+                    'author' => 'Sqwoz Bab',
+                    // свойства второго поста
+                ],
+            ];
+            
+        ?>
+    </div>
+    <div>
+        <?php 
+            
+            foreach ($posts as $post) {
+                include 'post_preview.php';
+            }
+
+        ?>
+    </div>
+</body>
+</html>
